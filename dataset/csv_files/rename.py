422788ddd7d7b2692c75ba96d144df523b5f4ce4
Chirag Tubakad <chirag.tubakad@Chirags-MacBook-Air.local>
2019-05-21 15:11:47 +0530

    Updated naming convention

--- dataset/csv_files/rename.py
+++ dataset/csv_files/rename.py
@@ -1,21 +1,17 @@
-# import os
-# location = '/Users/chirag.tubakad/Documents/Projects/Data-Completeness-/dataset/csv_files/temp'
 
 import os 
   
 # Function to rename multiple files 
-def main(): 
-    i = 1109
+def main():
       
-    for filename in os.listdir("/Users/chirag.tubakad/Documents/Projects/Data-Completeness-/dataset/csv_files/204/"): 
-        dst = str(i) + ".csv"
-        src ='/Users/chirag.tubakad/Documents/Projects/Data-Completeness-/dataset/csv_files/204/'+ filename 
-        dst ='/Users/chirag.tubakad/Documents/Projects/Data-Completeness-/dataset/csv_files/lol/'+ dst 
+    for filename in os.listdir("/Users/chirag.tubakad/Documents/Projects/temp/204/"): 
+        dst = "204_" + filename + ".csv"
+        src ='/Users/chirag.tubakad/Documents/Projects/temp/204/'+ filename 
+        dst ='/Users/chirag.tubakad/Documents/Projects/temp/CSV_All/'+ dst 
           
         # rename() function will 
         # rename all the files 
         os.rename(src, dst) 
-        i += 1
   
 # Driver Code 
 if __name__ == '__main__': 
